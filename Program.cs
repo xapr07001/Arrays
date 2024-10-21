6af315b1774054ebfe2045f695bbdd2683c8e11f
xapr07001 <xapr07001@utb.vaxjo.se>
2024-10-21 14:08:26 +0200

    tic tac toe

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿char[,] gameboard = new char[,]{{'-','-','-'}, {'-','-','-'}, {'-','-','-'}};
 bool win = false;
-bool p = false;
+bool player = false;
 while(!win){
 
-    Play(ref p);
-    p = !p;
+    Play(ref player);
+    player = !player;
 
     for(int x = 0; x < gameboard.GetLength(0);x++){
     for(int y = 0; y < gameboard.GetLength(1);y++){
@@ -13,6 +13,11 @@ while(!win){
     Console.WriteLine();
     }
     win = WinCheck();
+}
+if(player){
+    Console.WriteLine("Player 1 Wins");
+}else{
+    Console.WriteLine("Player 2 wins");
 }
 
 bool WinCheck(){
@@ -32,7 +37,7 @@ bool Winx(){
 }
 bool Winy(){
     for(int m = 0;m <= 2; m++){
-        if(gameboard[0, m] ==  '-'){
+        if(gameboard[m, 0] ==  '-'){
             continue;
         }
         if(gameboard[m, 0] == gameboard[m, 1] && gameboard[m, 1] == gameboard[m, 2]){
@@ -42,8 +47,10 @@ bool Winy(){
     return false;
 }
 bool WinDiagonal(){
-    if(gameboard[1, 1] == '-' )
+    if(gameboard[1, 1] == '-' ){
         return false;
+    }
+
 
     if(gameboard[0, 0] == gameboard[1, 1] && gameboard[1, 1] == gameboard[2, 2]){
         return true;
@@ -52,8 +59,6 @@ bool WinDiagonal(){
     }
     return false;
 }
-
-
 void Play(ref bool p){
     char ox;
     if(p){
@@ -61,14 +66,19 @@ void Play(ref bool p){
     }else{
         ox = 'o';
     }
+    
+    Console.WriteLine("Ange...");
+    int b = int.Parse(Console.ReadLine());
 
     Console.WriteLine("Ange...");
     int a = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Ange...");
-    int b = int.Parse(Console.ReadLine());
 
-    gameboard[a,b] = ox;
 
-    
+    if(gameboard[a-1,b-1] == '-'){
+        gameboard[a-1,b-1] = ox;
+    }else{
+        Console.WriteLine("not possible");
+        p = !p;
+    }
 }
--- obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79993146b5c722a5321819b6ed79cec2effb857a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f800fc0a688cbbc179d514c2c8be32f102d93079")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
